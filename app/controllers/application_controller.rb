6c6ebf9771c8ef1a52e1a7cd415db4355648ba87
sbwinter <swinter2011@gmail.com>
2014-12-02 16:35:08 -0700

    Add welcome

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  def welcome
+    render text: "Meet Franklin. Franklin helps you become the person you want to be."
+  end
 end
