48a0d01a9d28f0d29da62b155e82332bf2ef3560
sbwinter <swinter2011@gmail.com>
2014-12-02 19:17:16 -0700

    Static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,28 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  def setup
+    @base_title = "Franklin App"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
+    assert_select "a[href=?]", "/static_pages/about"
+    assert_select "a[href=?]", "/static_pages/contact"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | #{@base_title}"
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 
 end
